1b1e730376522909974e482164525ce4c482cf6e
Kot <umaduy@yandex.ru>
2024-01-24 08:09:09 +0300

    Рекурсия. Число фибоначи.

---
+++ Task_31.py
@@ -0,0 +1,13 @@
+# Задача №31. Последовательностью Фибоначчи называется последовательность чисел a0, a1, ..., an, ..., где a0 = 0, a1 = 1, ak = ak-1 + ak-2 (k > 1).
+# Требуется найти N-е число Фибоначчи
+# Input: 7
+# Output: 21
+
+
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+        
+n = int(input('Enter fibonacci number: '))
+print(fibonacci(n + 1))
